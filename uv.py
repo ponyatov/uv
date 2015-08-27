26227a16c4f50d69010a42b8e8a0d7ebb5cfe1ab
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-27 15:42:12 +0400

    * exit on [ESC]
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- uv.py
+++ uv.py
@@ -4,10 +4,9 @@ import cv2 as cv
 cam=cv.VideoCapture(0)
 
 cv.namedWindow('cam',cv.CV_WINDOW_AUTOSIZE)
-while cam.isOpened():
+while cam.isOpened() and cv.waitKey(1)!=27: # ESC
     ret,frame=cam.read()
     cv.imshow('cam',frame)
-    cv.waitKey(1)
 
 cam.release()
 cv.destroyAllWindows()
