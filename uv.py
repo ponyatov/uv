eeccf9d86b10b7cb94a958f92807858ef6002501
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-27 15:45:51 +0400

    * polished
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- uv.py
+++ uv.py
@@ -1,12 +1,9 @@
-
 import cv2 as cv
-
 cam=cv.VideoCapture(0)
-
-cv.namedWindow('cam',cv.CV_WINDOW_AUTOSIZE)
-while cam.isOpened() and cv.waitKey(1)!=27: # ESC
-    ret,frame=cam.read()
-    cv.imshow('cam',frame)
-
-cam.release()
-cv.destroyAllWindows()
+if cam.isOpened():
+    cv.namedWindow('cam')
+    while cv.waitKey(1)!=27: # ESC
+        ret,frame=cam.read()
+        cv.imshow('cam',frame)
+    cam.release()
+    cv.destroyAllWindows()
